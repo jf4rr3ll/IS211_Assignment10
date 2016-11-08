41a381a26cfcd44bd7d25f20646f7d6e6e23c4f3
jf4rr3ll <jf4rr3ll@gmail.com>
2016-11-07 22:30:50 -0500

    Update to query for pets database

--- query_pets.py
+++ query_pets.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """This module queries the pets database"""
+
+import sqlite3 as lite
+import sys
+
+person_ID = raw_input("Input Person ID: ")
+con = lite.connect('pets.db')
+
+with con:
+
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM pet "
+                "INNER JOIN person_pet"
+                "ON pet.id = person_pet.pet_id"
+                "INNER JOIN person"
+                "ON person_pet.person_id = person.id"
+                "WHERE person.id = ?", (person_ID))
+    rows = cur.fetchall()
+    for row in rows:
+        print "%s %s %s %s" % (row["id"], row["first_name"], row["last_name"], row["age"])
+        print "%s %s %s %s %s" % (row["id"], row["name"], row["breed"], row["age"], row["dead"])
